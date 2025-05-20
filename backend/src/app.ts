ff24e4c201a845a545029cd8a4f0c219777e7c4a
Maga <marcelomagario@gmail.com>
2025-05-20 20:29:56 -0300

    Setup backend server with DB

---
+++ backend/src/app.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001; 
+
+const pool = new Pool({
+user: process.env.DB_USER,
+host: process.env.DB_HOST,
+database: process.env.DB_DATABASE,
+password: process.env.DB_PASSWORD,
+port: parseInt(process.env.DB_PORT || '5432'), 
+});
+
+pool.connect((err, client, done) => {
+    if (err) {
+        console.error('Error trying to connect to Database:', err.stack);
+        return;
+    }
+    console.log('Connected sucessfully into PostgreSQL!');
+    if (client) { 
+        client.release(); 
+    }
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Portifolio API working properly!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
+export default pool;
